2bb21ab600e6de87c22530c2f9b81c4df17c2a68
Aaron Christian <60152828+aaronchristian99@users.noreply.github.com>
2021-02-12 19:31:36 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,188 @@
+let fieldsCheck;
+let formContainer = document.querySelector(".formContainer");
+let confirmContainer = document.querySelector(".confirmContainer");
+let legalContainer = document.querySelector(".legalContainer");
+
+
+function hasCharsCheck(dataToCheck){
+    let pattern = /^[a-zA-Z]+$/;
+
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function hasNumsCheck(dataToCheck){
+    let pattern = /^[0-9]+$/;
+
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function isEmailCheck(dataToCheck){
+    let pattern = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function isTelCheck(dataToCheck){
+    let pattern = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function isStreetCheck(dataToCheck){
+    let pattern = /^(?:[Pp][Oo]\s[Bb][Oo][Xx]|[0-9]+)\s(?:[0-9A-Za-z\.'#]|[^\S\r\n])+/;
+
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function isPostalCheck(dataToCheck){
+    let pattern = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
+
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function isDateCheck(dataToCheck){
+    let pattern = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function isCheckboxChecked(dataToCheck){
+    
+    if(dataToCheck.field.checked==true){
+        return true;
+    }
+
+    return false;
+}
+
+
+function errorsReset(){
+    fieldsCheck.forEach(inputField =>{
+        inputField.error.innerText = "";
+    });
+}
+
+
+function formChecker(e){
+    console.log("button clicked");
+    let errorsDetected = 0;
+    errorsReset();
+    e.preventDefault();
+
+    // automated error checking
+    fieldsCheck.forEach(inputField => {
+        if(inputField.checker(inputField)==false){
+            inputField.error.innerText = inputField.msg;
+            errorsDetected +=1;
+        }
+    });
+
+    if(errorsDetected==0){
+        console.log("no errors");
+        formContainer.style.display = "none";
+        confirmContainer.style.display = "block";
+        legalContainer.style.display = "none";
+    }
+
+
+}
+
+
+function initializeForm(){
+    
+    //grabbing our 12 input and error fields
+    let firstName = document.querySelector("#firstName");
+    let firstNameError = document.querySelector("#firstNameError");
+
+    let lastName = document.querySelector("#lastName");
+    let lastNameError = document.querySelector("#lastNameError");
+
+    let emailAddress = document.querySelector("#emailAddress");
+    let emailAddressError = document.querySelector("#emailAddressError");
+
+    let phoneNumber = document.querySelector("#phoneNumber");
+    let phoneNumberError = document.querySelector("#phoneNumberError");
+
+    let street = document.querySelector("#street1");
+    let streetError = document.querySelector("#street1Error");
+
+    let city = document.querySelector("#city");
+    let cityError = document.querySelector("#cityError");
+
+    let postalCode = document.querySelector("#postalCode");
+    let postalCodeError = document.querySelector("#postalCodeError");
+
+    let province = document.querySelector("#province");
+    let provinceError = document.querySelector("#provinceError");
+
+    let userDob = document.querySelector("#userDob");
+    let userDobError = document.querySelector("#userDobError");
+
+    let termsCheckbox = document.querySelector("#termsCheckbox");
+    let termsCheckboxError = document.querySelector("#termsCheckboxError");
+
+    //declaring our data object
+    fieldsCheck = [
+        {field: firstName, checker: hasCharsCheck, error: firstNameError, msg:"* Please enter a valid first name."},
+        {field: lastName, checker: hasCharsCheck, error: lastNameError, msg:"* Please enter a valid last name."},
+        {field: emailAddress, checker: isEmailCheck, error: emailAddressError, msg:"* Please enter a valid email."},
+        {field: phoneNumber, checker: isTelCheck, error: phoneNumberError, msg:"* Please enter a valid phone number."},
+        {field: street, checker: isStreetCheck, error: streetError, msg:"* Please enter a valid address line 1."},
+        {field: city, checker: hasCharsCheck, error: cityError, msg:"* Please enter a valid city."},
+        {field: postalCode, checker: isPostalCheck, error: postalCodeError, msg:"* Please enter a valid postal code."},
+        {field: province, checker: hasCharsCheck, error: provinceError, msg:"* Please select a valid province."},
+        {field: userDob, checker: isDateCheck, error: userDobError, msg:"* Please select a valid date of birth."},
+        {field: termsCheckbox, checker: isCheckboxChecked, error: termsCheckboxError, msg:"* Please accept terms to continue."},
+    ]
+
+    let logoLink = document.querySelector("#logo");
+    let formSubmit = document.querySelector("#formSubmitBtn");
+    let confirmationBtn = document.querySelector("#confirmationBtn");
+    let legalLink = document.querySelector("#legalLink");
+
+    formSubmit.addEventListener("click", formChecker);
+
+    confirmationBtn.addEventListener("click", function(){
+        formContainer.style.display = "block";
+        confirmContainer.style.display = "none";
+        legalContainer.style.display = "none";
+    });
+
+    legalLink.addEventListener("click", function(){
+        formContainer.style.display = "none";
+        confirmContainer.style.display = "none";
+        legalContainer.style.display = "block";
+    });
+
+    logoLink.addEventListener("click", function(){
+        formContainer.style.display = "block";
+        confirmContainer.style.display = "none";
+        legalContainer.style.display = "none";
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    initializeForm();
+})
